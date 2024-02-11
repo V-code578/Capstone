5a17b91bc016f64058c7f9c8c3bce2672214fc67
V-code578 <67007909+V-code578@users.noreply.github.com>
2024-02-11 20:31:48 +0530

    Products.cs

--- Products.cs
+++ Products.cs
@@ -48,6 +48,29 @@ namespace OnlineShopping.Models
     }
 }
 
+IProductRepo.cs:
+using OnlineShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopping.Repos
+{
+    public interface IProductRepo
+    {
+        Task<Product> GetProductById(int productId);
+        Task<Product> GetProductByName(string productName);
+        Task<List<Product>> GetAllProducts();
+        Task<List<Product>> GetProductsByCategory(int categoryId);
+        Task AddProduct(Product product);
+        Task UpdateProduct(int productId, Product product);
+        Task DeleteProduct(int productId);
+    }
+}
+
+
 EFProductRepo.cs:
 using Microsoft.EntityFrameworkCore;
 using OnlineShopping.Models;
@@ -168,6 +191,7 @@ namespace OnlineShopping.Repos
         }
     }
 }
+
 
 
 ProductController.cs:
